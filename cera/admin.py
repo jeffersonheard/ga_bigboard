cc88a9737dbdfa88a4a8c21f6fd72b95307b4ae9
Jeff Heard <jeff@renci.org>
2012-07-16 10:27:56 -0400

    oops

--- cera/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib.gis import admin
-from ga_app import models
-
-# admin.site.register(models.MyModel, admin_class=admin.OSMGeoAdmin)
--- cera/api.py
+++
@@ -1,5 +0,0 @@
-from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS	
-from tastypie.api import Api
-from tastypie.authentication import BasicAuthentication
-from tastypie.authhorization import Authorization
-from ga_ows.tastyhacks import GeoResource
